53b4fe276c3903984cf9a2bc1c55a728b2a6400e
Gejsi <vgejsi@gmail.com>
2023-07-13 14:55:07 +0200

    Added landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,43 @@
+import Link from 'next/link'
 import type { Page } from './_app'
+import Image from 'next/image'
 
 const Home: Page = () => {
   return (
-    <>
-      <h1 className='text-6xl'>Home page</h1>
-    </>
+    <section className='flex h-full flex-col items-center md:flex-row'>
+      <div className='prose w-full md:prose-xl lg:w-1/2'>
+        <h1>
+          The most{' '}
+          <span className='bg-gradient-to-r from-violet-500 to-fuchsia-500 bg-clip-text text-transparent'>
+            controversial
+          </span>{' '}
+          social media platform
+        </h1>
+
+        <p>
+          As Twitter's influence fades, Squealer emerges as a dynamic platform,
+          ready to redefine how we connect, engage, and share our lives.
+          Experience the freedom to express yourself, connect with like-minded
+          individuals, and make your voice heard in a digital realm driven by
+          genuine interactions.
+        </p>
+
+        <div className='flex w-full justify-center'>
+          <Link href='/public/all' className='btn-primary btn-lg btn'>
+            Checkout squeals
+          </Link>
+        </div>
+      </div>
+
+      <div className='relative hidden h-3/5 w-1/2 lg:block'>
+        <Image
+          src='/logo.png'
+          alt='Squealer logo'
+          fill
+          className='mask mask-hexagon w-1/2 object-contain'
+        />
+      </div>
+    </section>
   )
 }
 
